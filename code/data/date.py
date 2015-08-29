b6ac872ce094cc7c679c3feaf722c1a43d47762e
Satya Prakash Panuganti <yoda101@github.com>
2015-08-29 13:24:32 +0530

    few changes. I was just messing aroud a little

--- code/data/date.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-urllib2.install_opener(
-    urllib2.build_opener(
-        urllib2.ProxyHandler({'http': '127.0.0.1'})
-    )
-)
-urllib2.urlopen('http://webmail.iitk.ac.in/')
--- code/data/time.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-	date | sponge ~/workspace/svn/Takneek/devel/code/time.dat
-	sleep 0.1s
-done
