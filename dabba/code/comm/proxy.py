53d8a9daa11654be2d69370e5c4515b309ae1395
honeypc <amanabt@iitk.ac.in>
2015-08-30 17:56:38 +0530

    user and password removed from proxy.py

--- dabba/code/comm/proxy.py
+++ dabba/code/comm/proxy.py
@@ -1,6 +1,6 @@
 import urllib2
 
-proxy = urllib2.ProxyHandler({'http': 'http://amanabt:IITKaat@vsnlproxy.iitk.ac.in:3128'})
+proxy = urllib2.ProxyHandler({'http': 'http://username:password@vsnlproxy.iitk.ac.in:3128'})
 auth = urllib2.HTTPBasicAuthHandler()
 opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
 urllib2.install_opener(opener)
